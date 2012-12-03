6271512bc11ea204b1c81407d3f76dbac3707ad4
Mike Wolfson <mikewolfson@airstrip.com>
2012-12-03 09:39:43 -0700

    Using an Activity to perform business logic

--- src/com/andevcon/bootcamp/MainActivity.java
+++ src/com/andevcon/bootcamp/MainActivity.java
@@ -2,7 +2,12 @@ package com.andevcon.bootcamp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final EditText expenseName = (EditText) findViewById(R.id.item_name_edittext);
+		final EditText expenseAmount = (EditText) findViewById(R.id.item_amount_edittext);
+		Button nextButton = (Button) findViewById(R.id.enter_button);
+		
+		nextButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String name = expenseName.getText().toString();
+				String amount = expenseAmount.getText().toString();
+				
+				Log.v("Bootcamp", "The expense: " + name + " has the value: " + amount);
+			}
+		});
+
 	}
 
 	@Override
