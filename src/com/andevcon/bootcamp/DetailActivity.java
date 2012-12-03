ad67f27bd5e902c612d1ea2f812c03f279656f91
Mike Wolfson <mikewolfson@airstrip.com>
2012-12-03 09:48:34 -0700

    ActionBar implementation

--- src/com/andevcon/bootcamp/DetailActivity.java
+++ src/com/andevcon/bootcamp/DetailActivity.java
@@ -4,24 +4,93 @@
 package com.andevcon.bootcamp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
 
 /**
  *
  */
 public class DetailActivity extends Activity {
-
+	private ShareActionProvider provider;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setTitle(R.string.expense_detail);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
+		getMenuInflater().inflate(R.menu.detail_menu, menu);
+		
+		//Setup share functionality
+		MenuItem item = menu.findItem(R.id.menu_share);
+		provider = (ShareActionProvider) item.getActionProvider();
+		setShareIntent();
+		
 		return true;
+	}
+
+	/* Handles item selections */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent homeIntent = new Intent(this, MainActivity.class);
+			startActivity(homeIntent);
+			return true;
+		case R.id.menu_main:
+			Intent mainIntent = new Intent(this, MainActivity.class);
+			startActivity(mainIntent);
+			return true;
+		case R.id.menu_email:
+			String[] mailto = new String[] { "adcbootcamp@email.com" };
+			String subject = "Subject: ";
+			String body = "Body:";
+
+			Intent email = new Intent(Intent.ACTION_SEND);
+			email.setType("message/rfc822");
+			email.putExtra(Intent.EXTRA_EMAIL, mailto);
+			email.putExtra(Intent.EXTRA_SUBJECT, subject);
+			email.putExtra(Intent.EXTRA_TEXT, body);
+
+			try {
+				startActivity(email);
+			} catch (Exception e) {
+				Context context = getApplicationContext();
+				String text = "Oops, does your email work?";
+				int duration = Toast.LENGTH_LONG;
+
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+	
+	public void setShareIntent() {
+		String shareSubject = "Expense to share: ";
+		String shareText = "Expense info: ";
+
+		final Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_SUBJECT, shareSubject);
+		intent.putExtra(Intent.EXTRA_TEXT, shareText);
+		if (provider != null) {
+			provider.setShareIntent(intent);
+		} 
 	}
 }
